1628ccf6385b854ffd30ac93993410d3145c1148
vaibhavt-nt <vaibhavti.nt@gmail.com>
2024-02-19 10:34:07 +0530

    Stack widget (WIP).

--- lib/widgets/scroll_bar.dart
+++ lib/widgets/scroll_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ScrollBar extends StatelessWidget {
-   ScrollBar({super.key});
+  ScrollBar({super.key});
 
   final ScrollController _firstController = ScrollController();
 
@@ -9,54 +9,50 @@ class ScrollBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-          return Row(
-            children: <Widget>[
-              SizedBox(
-                  width: constraints.maxWidth / 2,
-                  // Only one scroll position can be attached to the
-                  // PrimaryScrollController if using Scrollbars. Providing a
-                  // unique scroll controller to this scroll view prevents it
-                  // from attaching to the PrimaryScrollController.
-                  child: Scrollbar(
-                    thumbVisibility: true,
+      return Row(
+        children: <Widget>[
+          SizedBox(
+              width: constraints.maxWidth / 2,
+              // Only one scroll position can be attached to the
+              // PrimaryScrollController if using Scrollbars. Providing a
+              // unique scroll controller to this scroll view prevents it
+              // from attaching to the PrimaryScrollController.
+              child: Scrollbar(
+                thumbVisibility: true,
+                controller: _firstController,
+                child: ListView.builder(
                     controller: _firstController,
-                    child: ListView.builder(
-                        controller: _firstController,
-                        itemCount: 100,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Padding(
+                    itemCount: 100,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Scrollable 1 : Index $index'),
+                      );
+                    }),
+              )),
+          SizedBox(
+              width: constraints.maxWidth / 2,
+              // This vertical scroll view has not been provided a
+              // ScrollController, so it is using the
+              // PrimaryScrollController.
+              child: Scrollbar(
+                thumbVisibility: true,
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                          height: 50,
+                          color: index.isEven
+                              ? Colors.amberAccent
+                              : Colors.blueAccent,
+                          child: Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Text('Scrollable 1 : Index $index'),
-                          );
-                        }
-                    ),
-                  )
-              ),
-              SizedBox(
-                  width: constraints.maxWidth / 2,
-                  // This vertical scroll view has not been provided a
-                  // ScrollController, so it is using the
-                  // PrimaryScrollController.
-                  child: Scrollbar(
-                    thumbVisibility: true,
-                    child: ListView.builder(
-                        itemCount: 100,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                              height: 50,
-                              color: index.isEven ? Colors.amberAccent : Colors.blueAccent,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text('Scrollable 2 : Index $index'),
-                              )
-                          );
-                        }
-                    ),
-                  )
-              ),
-            ],
-          );
-        }
-    );
+                            child: Text('Scrollable 2 : Index $index'),
+                          ));
+                    }),
+              )),
+        ],
+      );
+    });
   }
 }
