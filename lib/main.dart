edc0034a0f46f3d21b7ddcf1ab43a8918d4f8019
vaibhavt-nt <vaibhavti.nt@gmail.com>
2024-02-20 10:40:36 +0530

    SliverWidget  learned

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ import 'package:flutter_training/widgets/refresh_indicator.dart';
 import 'package:flutter_training/widgets/scroll_bar.dart';
 import 'package:flutter_training/widgets/silver_appbar.dart';
 import 'package:flutter_training/widgets/slider.dart';
+import 'package:flutter_training/widgets/slivergrid.dart';
 import 'package:flutter_training/widgets/sliverlist.dart';
+import 'package:flutter_training/widgets/streambuilder.dart';
 import 'package:flutter_training/widgets/tab_bar.dart';
 import 'package:flutter_training/widgets/table_view.dart';
 
@@ -26,7 +28,7 @@ class homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "my home page",
-      home: SliverListWidget(),
+      home: StreamBuilderWidget(),
     );
   }
 }
